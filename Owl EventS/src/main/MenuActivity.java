387e866d8f8c501e626293d1cc414649701479ea
wenxuancai <wenxuancai1992@gmail.com>
2014-01-25 15:36:51 -0600

    id

--- Owl EventS/src/main/MenuActivity.java
+++ Owl EventS/src/main/MenuActivity.java
@@ -17,6 +17,7 @@
 package main;
 
 import com.google.android.glass.sample.compass.R;
+import com.parse.Parse;
 
 import android.app.Activity;
 import android.content.ComponentName;
@@ -56,6 +57,7 @@ public class MenuActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bindService(new Intent(this, EventService.class), mConnection, 0);
+        Parse.initialize(this, "FiJwGLjq2YlT3ZNS0XR0l4stnsoTznGyBtPGtNQq", "spVdi98WR76oqNwxF2opcyIXa2yo0Oysl3I4zpA6");
     }
 
     @Override
